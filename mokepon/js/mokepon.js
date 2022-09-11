6da3a61407ec62b3b27b840ad194904915972995
jtobar <cuentamejorgerenovar@gmail.com>
2022-09-11 09:35:51 -0500

    incio mokepon

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,4 @@
+
+function seleccionarMascotaJugador(){alert('SELECCIONASTE TU MASCOTA')}
+let botonMascotaJugador=document.getElementById('boton-mascota')
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
